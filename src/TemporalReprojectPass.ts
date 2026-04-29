a41ad7bd44adcfcd77b1e7f9ee8546430bc15524
KallkaGo <765830637@qq.com>
2026-04-29 20:10:07 +0800

    feat: BiCubicCatmullRom5Tap

--- src/TemporalReprojectPass.ts
+++ src/TemporalReprojectPass.ts
@@ -303,6 +303,52 @@ function createResolveMaterial(): ShaderMaterial {
         return historyColor;
       }
 
+      vec4 BiCubicCatmullRom5Tap(sampler2D tex, vec2 P, vec2 invTexSize) {
+        vec2 Weight[3];
+        vec2 Sample[3];
+
+        vec2 UV = P / invTexSize;
+        vec2 tc = floor(UV - 0.5) + 0.5;
+        vec2 f = UV - tc;
+        vec2 f2 = f * f;
+        vec2 f3 = f2 * f;
+
+        vec2 w0 = f2 - 0.5 * (f3 + f);
+        vec2 w1 = 1.5 * f3 - 2.5 * f2 + vec2(1.0);
+        vec2 w3 = 0.5 * (f3 - f2);
+        vec2 w2 = vec2(1.0) - w0 - w1 - w3;
+
+        Weight[0] = w0;
+        Weight[1] = w1 + w2;
+        Weight[2] = w3;
+
+        Sample[0] = tc - vec2(1.0);
+        Sample[1] = tc + w2 / max(Weight[1], vec2(1e-6));
+        Sample[2] = tc + vec2(2.0);
+
+        Sample[0] *= invTexSize;
+        Sample[1] *= invTexSize;
+        Sample[2] *= invTexSize;
+
+        float sampleWeight[5];
+        sampleWeight[0] = Weight[1].x * Weight[0].y;
+        sampleWeight[1] = Weight[0].x * Weight[1].y;
+        sampleWeight[2] = Weight[1].x * Weight[1].y;
+        sampleWeight[3] = Weight[2].x * Weight[1].y;
+        sampleWeight[4] = Weight[1].x * Weight[2].y;
+
+        vec4 Ct = texture2D(tex, vec2(Sample[1].x, Sample[0].y)) * sampleWeight[0];
+        vec4 Cl = texture2D(tex, vec2(Sample[0].x, Sample[1].y)) * sampleWeight[1];
+        vec4 Cc = texture2D(tex, vec2(Sample[1].x, Sample[1].y)) * sampleWeight[2];
+        vec4 Cr = texture2D(tex, vec2(Sample[2].x, Sample[1].y)) * sampleWeight[3];
+        vec4 Cb = texture2D(tex, vec2(Sample[1].x, Sample[2].y)) * sampleWeight[4];
+
+        float weightSum = sampleWeight[0] + sampleWeight[1] + sampleWeight[2] + sampleWeight[3] + sampleWeight[4];
+        float weightMultiplier = 1.0 / max(weightSum, 1e-6);
+
+        return max((Ct + Cl + Cc + Cr + Cb) * weightMultiplier, vec4(0.0));
+      }
+
       void main() {
         vec3 currentColor = texture2D(tColor, vUv).rgb;
 
@@ -324,7 +370,7 @@ function createResolveMaterial(): ShaderMaterial {
           return;
         }
 
-        vec3 historyColor = texture2D(tHistory, historyUV).rgb;
+        vec3 historyColor = BiCubicCatmullRom5Tap(tHistory, historyUV, uInvTexSize).rgb;
         vec3 currentTonemappedYCoCg = RGBtoYCoCg(ToneMapSimple(currentColor));
         vec3 historyTonemappedYCoCg = RGBtoYCoCg(ToneMapSimple(historyColor));
 
